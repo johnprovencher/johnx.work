d4cfb415d82eff5687cc87be869507b22756523d
dataexcess <admin@dataexcess.xyz>
2023-07-24 17:49:43 +0200

    Some more undefined checks on selectedToken

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -221,7 +221,7 @@ const ProjectDetails = ({ contractAddress, id }: Props) => {
                         </Box>
                         <Box sx={{display: 'inline-flex', position: 'relative', backgroundColor:'black', color:'white'}}>
                             {
-                                selectedToken && (
+                                selectedToken && selectedToken !== undefined && (
                                     <Typography sx={{position: 'relative', backgroundColor: 'black' }}> {selectedToken.tokenID} </Typography>
                                 )
                             }
@@ -229,14 +229,14 @@ const ProjectDetails = ({ contractAddress, id }: Props) => {
                     </Box>
                     <Box sx={{width: '100%', px: '2em'}}>
                         {
-                            selectedToken && (
+                            selectedToken && selectedToken !== undefined && (
                                 <TokenImage contractAddress={project.contract.id} tokenId={selectedToken.tokenID} aspectRatio={project.aspectRatio || parseAspectRatio(project.scriptJSON)} />
                             )
                         }
                     </Box>
                     <Box>
                         {
-                            selectedToken && selectedToken?.traits.map((trait:Trait, idx:number) => (
+                            selectedToken && selectedToken !== undefined && selectedToken?.traits.map((trait:Trait, idx:number) => (
                                 <Box key={idx} sx={{ position: 'relative', display: 'flex', width: '100%', gap: '2em', justifyContent: 'space-between' }}>
                                     <JohnDotsLine />
                                     <Box sx={{display: 'inline-flex', position: 'relative', backgroundColor:'black'}}>
@@ -254,7 +254,7 @@ const ProjectDetails = ({ contractAddress, id }: Props) => {
                             live view
                         </JohnBox>
                         <JohnBox onClick={ () =>
-                            window.open(`https://${isUsingTestnet() ? 'testnets.' : ''}opensea.io/assets/${isUsingTestnet() ? 'goerli' : 'ethereum'}/${selectedToken ? contractAddress?.toLowerCase() + '/' + selectedToken.tokenID : ''}`)
+                            window.open(`https://${isUsingTestnet() ? 'testnets.' : ''}opensea.io/assets/${isUsingTestnet() ? 'goerli' : 'ethereum'}/${ (selectedToken && selectedToken !== undefined) ? contractAddress?.toLowerCase() + '/' + selectedToken.tokenID : ''}`)
                         }>
                             opensea
                         </JohnBox>
